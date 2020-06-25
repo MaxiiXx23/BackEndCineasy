a5a0ccfc655dac22c1c66818ae1b83b30678dda9
Otavio Barreto de Oliveira Silva <otaviosilva2632@gmail.com>
2020-06-25 19:20:12 -0300

    Conectando com o banco de dados

--- models/conexao.js
+++ models/conexao.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const conn = mysql.createPool({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'Cineasy'
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b1e63faf60a2af',
+  password: '28afff17',
+  database: 'heroku_7fd597d2fb977e3'
 });
 module.exports = conn;
