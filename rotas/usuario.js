5a00982a69693afdb643d94dc0261ba9e423946a
Felipe Augustos <47693479+userAugustos@users.noreply.github.com>
2020-06-26 11:01:00 -0300

    Update usuario.js

--- rotas/usuario.js
+++ rotas/usuario.js
@@ -264,7 +264,7 @@ router.post('/login', (req, res, next) => {
                     })
                     return res.status(200).send({
                         mensagem: 'Login feito com sucesso!',
-                        token, id: results[0].id_user, nome: results[0].nome
+                        token, id: results[0].id_user, nome: results[0].nome, tipo_user: results[0].tipo_user
                     })
                 }
                 return res.status(401).send({ mensagem: 'Falha na autenticação' })
